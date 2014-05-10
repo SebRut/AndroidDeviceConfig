0cde73f7bbcca1a72e280b83908e1a75338ca007
SebRut <sebastian.rutofski@gmail.com>
2014-05-10 11:29:05 +0200

    UI stuff

---
+++ AndroidDeviceConfig.WpfTool/DeviceConfigViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using AndroidDeviceConfig.WpfTool.Annotations;
+
+namespace AndroidDeviceConfig.WpfTool
+{
+    class DeviceConfigViewModel : INotifyPropertyChanged
+    {
+        private DeviceConfig _Model;
+
+        public DeviceConfig Model
+        {
+            get { return _Model; }
+        }
+
+        public DeviceConfigViewModel(DeviceConfig model)
+        {
+            _Model = model;
+
+            foreach (DeviceVersion deviceVersion in _Model.Versions)
+            {
+                _Versions.Add(deviceVersion);
+            }
+
+            _Versions.CollectionChanged += _Versions_CollectionChanged;
+        }
+
+        void _Versions_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            _Model.Versions = new List<DeviceVersion>(_Versions);
+            OnPropertyChanged("Versions");
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Vendor
+        {
+            get { return Model.Vendor; }
+            set
+            {
+                if (Vendor == value) return;
+                _Model.Vendor = value;
+                OnPropertyChanged("Vendor");
+            }
+        }
+
+        private string _Name;
+        public string Name
+        {
+            get { return Model.Name; }
+            set
+            {
+                if (Name == value) return;
+                _Model.Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public ObservableCollection<DeviceVersion> Versions
+        {
+            get { return _Versions; }
+            set
+            {
+                if (_Versions == value) return;
+                _Versions = value;
+                OnPropertyChanged("Versions");
+            }
+        }
+        private ObservableCollection<DeviceVersion> _Versions = new ObservableCollection<DeviceVersion>();
+    }
+}
---
+++ AndroidDeviceConfig.WpfTool/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Windows.Input;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using Microsoft.Win32;
+
+namespace AndroidDeviceConfig.WpfTool
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private DeviceConfig _config = new DeviceConfig();
+        private string path = String.Empty;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new DeviceConfigViewModel(_config);
+        }
+
+        private void CommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private async void OpenConfig_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "XML Files|*.xml";
+            ofd.Multiselect = false;
+            ofd.ShowDialog();
+
+            if (File.Exists(ofd.FileName))
+            {
+                bool failure = false;
+                try
+                {
+                    _config = DeviceConfig.LoadConfig(ofd.FileName);
+                    this.DataContext = new DeviceConfigViewModel(_config);
+                    path = ofd.FileName;
+                }
+                catch
+                {
+                    failure = true;
+                }
+                if (failure) await this.ShowMessageAsync("Something went wrong", "Config invalid.");
+            }
+        }
+
+        private void SaveConfig_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.AddExtension = true;
+            sfd.Filter = "XML Files|*.xml";
+            sfd.FileName = path;
+            sfd.ShowDialog();
+
+            DeviceConfig.SaveConfig(sfd.FileName, ((DeviceConfigViewModel)DataContext).Model);
+        }
+    }
+}
