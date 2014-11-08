bcf9758e57e26bd894be6f9d01926eae281c7010
Sebastian Rutofski <sebrut@web.de>
2014-11-08 13:55:57 +0100

    tests added

---
+++ AndroidDeviceConfig.Test/TestDeviceConfig.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AndroidDeviceConfig.Test
+{
+    [TestFixture]
+    public class TestDeviceConfig
+    {
+        [Test]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void TestLoadConfigForFileNotFoundException()
+        {
+            var config = DeviceConfig.LoadConfig("specificFile.xml");
+        }
+
+        [Test]
+        public void TestSaveEmptyConfig()
+        {
+            DeviceConfig config = new DeviceConfig();
+            config.SaveConfig("config.xml");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (File.Exists("config.xml"))
+                File.Delete("config.xml");
+        }
+    }
+}
